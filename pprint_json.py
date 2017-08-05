e7d339d6bf32df8690917568d346f757843094b2
gmklenevskiy <gmklenevskiy@gmail.com>
2017-08-05 12:30:05 +0300

    сделано

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as reader:
+        parsed = json.load(reader)
+        pretty_print_json(parsed)
 
 
 def pretty_print_json(data):
-    pass
-
-
+    print(json.dumps(data, indent = 4, ensure_ascii=False, sort_keys=True))
+    
+    
+    
 if __name__ == '__main__':
-    pass
+    if len(sys.argv)>1:
+        filepath = sys.argv[1]
+        load_data(filepath)
+    else:
+        print("Please, enter the filepath")
